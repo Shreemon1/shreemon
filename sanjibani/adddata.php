c9c64fa7b09221720c3d6a853c8c68abafab5a81
Shreemon1 <sshreemon@gmail.com>
2020-10-12 06:30:22 -0700

    first website

---
+++ sanjibani/adddata.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST['name'];
+$mail=$_POST['mail'];
+$phone= $_POST['number'];
+$message = $_POST['message'];
+
+echo "  name is  ( " .$name , ")   number is  ( ".$phone, ")  mail is  (".$mail , ")  message is  (".$message , ")"
+?>
